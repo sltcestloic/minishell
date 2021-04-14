f8c10ce1d2a4a87f81f3f0830a486c0be9824908
Loïc <lbertran@student.42lyon.fr>
2021-04-14 10:40:42 +0200

    exit command

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 14:21:25 by lbertran          #+#    #+#             */
-/*   Updated: 2021/04/13 14:22:36 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/04/14 10:38:08 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	parse_input(char *input)
 {
+	if (ft_strcmp("exit", input) == 0)
+		exit(0);
 	printf("input: %s\n", input);
 }
