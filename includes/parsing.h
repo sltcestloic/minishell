5edc79a05daedd478b3b31b65d2f497a581023ba
Loïc <lbertran@student.42lyon.fr>
2021-04-22 12:34:21 +0200

    arg split now handles quotes

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 14:16:13 by lbertran          #+#    #+#             */
-/*   Updated: 2021/04/17 15:33:37 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/04/22 12:30:06 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 typedef struct s_command
 {
-				char	*name;
 				char	**args;
 				t_shell	*shell;
 }				t_command;
@@ -25,6 +24,7 @@ typedef struct s_split
 {
 			int	quote1;
 			int	quote2;
+			int	indx;
 }				t_split;
 
 void	parse_input(char *input, t_shell *shell);
@@ -37,6 +37,6 @@ void	parse_echo(t_command *cmd);
 
 
 int		count_cmds(const char *str, char c);
-char	**ft_splitcmds(const char *str, char c);
+char	**ft_splitcmds(const char *str, char c, int q);
 
 #endif
--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -1,29 +1,5 @@
 #include "minishell.h"
 
-char	*get_command_name(char *input)
-{
-	char	*ret;
-	char	**split;
-	int		i;
-
-	i = 0;
-	if (*input)
-	{
-		split = ft_split(input, ' ');	
-		ret = malloc(sizeof(char) * (ft_strlen(split[0]) + 1));
-		while (split[0][i])
-		{
-			ret[i] = split[0][i];
-			i++;
-		}
-		ret[i] = '\0';
-		free_split(split);
-	}
-	else
-		return (input);
-	return (ret);
-}
-
 char	*get_env_var(t_shell *shell, char *str)
 {
 	(void)shell;
@@ -42,12 +18,12 @@ char	**get_command_args(char	*input, t_shell *shell)
 	int		i;
 	int		j;
 
-	i = 1;
+	i = 0;
 	j = 0;
 	if (!*input)
 		return (NULL);
-	split = ft_split(input, ' ');
-	ret = malloc(sizeof(char *) * ft_splitlen(split));
+	split = ft_splitcmds(input, ' ', 1);
+	ret = malloc(sizeof(char *) * (ft_splitlen(split) + 1));
 	while (split[i])
 	{
 		if (split[i][0] == '$')
@@ -72,7 +48,6 @@ t_command	parse_command(char *input, t_shell *shell)
 {
 	t_command	cmd;
 
-	cmd.name = get_command_name(input);
 	cmd.args = get_command_args(input, shell);
 	cmd.shell = shell;
 	return (cmd);
@@ -83,33 +58,30 @@ void	handle_cmd(char *input, t_shell *shell)
 	t_command	cmd;
 
 	cmd = parse_command(input, shell);
-	if (ft_strcmp("exit", cmd.name) == 0)
+	if (ft_strcmp("exit", cmd.args[0]) == 0)
 		ft_exit(shell->to_free);
-	else if (ft_strcmp("echo", cmd.name) == 0)
+	else if (ft_strcmp("echo", cmd.args[0]) == 0)
 		parse_echo(&cmd);
-	else if (ft_strcmp("env", cmd.name) == 0)
+	else if (ft_strcmp("env", cmd.args[0]) == 0)
 		env(shell->env_var);
-	else if (ft_strcmp("export", cmd.name) == 0)
+	else if (ft_strcmp("export", cmd.args[0]) == 0)
 	{
 		if (!cmd.args[0])
 			export(shell->env_var);
 		else
-			new_env_elem(cmd.args[0], shell);
+			new_env_elem(cmd.args[1], shell);
 	}
-	else if (ft_strcmp("cd", cmd.name) == 0)
+	else if (ft_strcmp("cd", cmd.args[0]) == 0)
 	{
-		if (!cmd.args[0])
+		if (!cmd.args[1])
 			change_pwd(shell, NULL);
 		else if (ft_splitlen(cmd.args) > 1)
 			write(1, "cd: too many arguments\n", 24);
 		else
 			change_pwd(shell, cmd.args[0]);
 	}
-	else if (ft_strcmp("pwd", cmd.name) == 0)
+	else if (ft_strcmp("pwd", cmd.args[0]) == 0)
 		pwd(shell);
-	free(cmd.name);
-	if (cmd.args)
-		free_split(cmd.args);
 }
 
 void	parse_input(char *input, t_shell *shell)
@@ -118,7 +90,7 @@ void	parse_input(char *input, t_shell *shell)
 	int		i;
 
 	i = 0;
-	split = ft_splitcmds(input, ';');
+	split = ft_splitcmds(input, ';', 0);
 	while (split[i])
 	{
 		handle_cmd(split[i], shell);
