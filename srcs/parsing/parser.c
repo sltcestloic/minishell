988c140dee0772c4e9b97bc5de5b0c58e661f5a9
Loïc <lbertran@student.42lyon.fr>
2021-04-28 15:05:19 +0200

    fix print env var entre quote sans espace

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -5,14 +5,16 @@ char	*get_env_var(t_shell *shell, char *str)
 	t_envlst	*lst;
 	char		*ret;
 	char		**split;
+	char		*name;
 
 	split = ft_split(str, ' ');
+	name = ft_strtrim(&split[0][1], " \"'");
 	lst = shell->env_var;
 	ret = malloc(1);
 	*ret = 0;
 	while (lst)
 	{
-		if (ft_strcmp(lst->name, &split[0][1]) == 0)
+		if (ft_strcmp(lst->name, name) == 0)
 		{
 			free(ret);
 			ret = ft_strdup(lst->value);
@@ -107,7 +109,7 @@ void	cat_var(t_parser *parser, char *var, char *input, t_index *indx)
 	indx->k = ft_strlen(parser->parsed_input);
 	while (input[indx->i] && input[indx->i] != ' ')
 		indx->i++;
-	indx->i--;
+	indx->i -= 2;
 }
 
 void	add_separator(t_parser *parser, int indx)
@@ -153,7 +155,7 @@ int	treat_input(t_shell *shell, char *input, t_parser *parser)
 			else
 			{
 				free(var);
-				while (ft_isalnum(input[i.i + 1]) && input[i.i + 1])
+				while (input[i.i + 1] && ft_isalnum(input[i.i + 1]))
 					i.i++;
 			}
 		}
