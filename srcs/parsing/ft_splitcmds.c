bbfc52b972fa3ff8a9f850165bcc941daf642ce7
Loïc <lbertran@student.42lyon.fr>
2021-05-19 14:46:37 +0200

    le parsing me rend fou

--- srcs/parsing/ft_splitcmds.c
+++ srcs/parsing/ft_splitcmds.c
@@ -15,6 +15,22 @@ static int	get_type(char c)
 		return (-1);
 }
 
+void print_cmd(t_cmd *cmd)
+{
+	t_cmd *current = cmd; 									/*			*/
+	while (current)											/*			*/
+	{														/*			*/
+		printf("type: %d\n", current->type);				/*			*/
+		if (current->value)									/*			*/
+		{													/*   DEBUG	*/
+			printf("value:\n");								/*			*/
+			for (int k = 0; current->value[k]; k++)			/*			*/
+				printf("%s\n", current->value[k]);			/*			*/
+		}													/*			*/
+		current = current->next;							/*			*/
+	} 														/*			*/
+}
+
 t_cmd	*ft_argdup(const char *str, int start, int end)
 {
 	t_cmd	*cmd;
@@ -39,13 +55,34 @@ t_cmd	*ft_argdup(const char *str, int start, int end)
 	ret = swap;
 	cmd->type = 1;
 	cmd->value = ft_split(ret, ' ');
-	if (get_type(str[end + 1]) != -1)
-	{
-		cmd->next = malloc(sizeof(t_cmd));
-		cmd->next->type = get_type(str[end + 1]);
-	}
 	free(ret);
 	return (cmd);
+}
+
+char	**get_content(const char *str, int start, int end)
+{
+	char	*ret;
+	char	*swap;
+	char	**rett;
+	int		i;
+
+	i = 0;
+	ret = malloc(sizeof(char) * (end - start + 2));
+	if (!ret)
+		return (NULL);
+	while (start <= end)
+	{
+		ret[i] = str[start];
+		i++;
+		start++;
+	}
+	ret[i] = '\0';
+	swap = ft_strtrim(ret, " ");
+	free(ret);
+	ret = swap;
+	rett = ft_split(ret, ' ');
+	free(ret);
+	return (rett);
 }
 
 int	count_cmds(t_parser *parser)
@@ -61,31 +98,38 @@ int	count_cmds(t_parser *parser)
 t_cmd	*ft_splitcmds(const char *str, t_parser *parser)
 {
 	t_cmd	*cmd;
+	t_cmd	*next;
 	t_index	i;
 
 	cmd = malloc(sizeof(t_cmd));
 	cmd->type = -1;
+	cmd->next = NULL;
 	i = init_index();
 	i.i = -1;
 	if (!str)
 		return (NULL);
 	while (parser->separators[++i.i])
 	{
-		cmd_addback(cmd, ft_argdup(str, i.j, parser->separators[i.i] - 1));
+		int last = cmd_last_type(cmd);
+		if (last == 2 || last == 3)
+			cmd_last(cmd)->value = get_content(str, i.j, parser->separators[i.i] - 1);
+		else
+		{
+			cmd_addback(cmd, ft_argdup(str, i.j, parser->separators[i.i] - 1));
+			if (get_type(str[parser->separators[i.i]]) != -1)
+			{
+				next = malloc(sizeof(t_cmd));
+				next->type = get_type(str[parser->separators[i.i]]);
+				cmd_addback(cmd, next);
+			}
+		}
 		i.j = parser->separators[i.i] + 1;
 	}
-	cmd_addback(cmd, ft_argdup(str, i.j, ft_strlen(str)));
-	/* t_cmd *current = cmd; //DEBUG
-	while (current)
-	{
-		printf("type: %d\n", current->type);
-		if (current->value)
-		{
-			printf("value:\n");
-			for (int k = 0; current->value[k]; k++)
-				printf("%s\n", current->value[k]);
-		}
-		current = current->next;
-	} */
+	int last = cmd_last_type(cmd);
+	if (last == 2 || last == 3)
+		cmd_last(cmd)->value = get_content(str, i.j, ft_strlen(str));
+	else
+		cmd_addback(cmd, ft_argdup(str, i.j, ft_strlen(str)));
+	print_cmd(cmd);
 	return (cmd);
 }
--- srcs/util/cmd.c
+++ srcs/util/cmd.c
@@ -11,13 +11,8 @@ void	cmd_addback(t_cmd *cmd, t_cmd *add)
 			break ;
 		current = current->next;
 	}
+	add->next = NULL;
 	current->next = add;
-}
-
-void	cmd_addfront(t_cmd *cmd, t_cmd *add)
-{
-	add->next = cmd;
-	cmd = add;
 }
 
 void	cmd_free(t_cmd *cmd)
@@ -33,4 +28,32 @@ void	cmd_free(t_cmd *cmd)
 		current = current->next;
 	}
 	free(current);
+}
+
+int		cmd_last_type(t_cmd *cmd)
+{
+	t_cmd	*current;
+
+	current = cmd;
+	while (1)
+	{
+		if (!current->next)
+			break ;
+		current = current->next;
+	}
+	return (current->type);
+}
+
+t_cmd	*cmd_last(t_cmd *cmd)
+{
+	t_cmd	*current;
+
+	current = cmd;
+	while (1)
+	{
+		if (!current->next)
+			break ;
+		current = current->next;
+	}
+	return (current);
 }
