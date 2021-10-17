1b5d9c74464d00b3838657a0c05995d1a2ca7155
Loïc <lbertran@student.42lyon.fr>
2021-10-17 13:46:55 +0200

    add 0 support, fix empty env var crash, add invalid command check

--- srcs/parsing/env_var.c
+++ srcs/parsing/env_var.c
@@ -6,6 +6,9 @@ char	*get_env_var(t_shell *shell, char *name)
 	char		*ret;
 
 	lst = shell->env_var;
+	printf("%s\n", name);
+	if (ft_strcmp(name, "$?") == 0)
+		return (ft_itoa(shell->last_exit_return));
 	while (lst)
 	{
 		if (ft_strcmp(lst->name, name) == 0)
