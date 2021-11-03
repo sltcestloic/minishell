cd6b56c689f654c5a420186a6496632fa65b767b
Lucas Bourret <bourretlucas@gmail.com>
2021-11-03 14:05:54 +0100

    ok

--- libft/string/ft_strjoin.c
+++ libft/string/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: lubourre <lubourre@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 13:26:41 by lbertran          #+#    #+#             */
-/*   Updated: 2021/10/26 10:26:43 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/11/03 12:46:37 by lubourre         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,35 @@ char	*ft_strjoin(const char *s1, const char *s2, t_free *to_free)
 		while (s2[j])
 			ret[i++] = s2[j++];
 	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+	char	*ret;
+​
+	i = 0;
+	j = 0;
+	if (!s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (NULL);
+	if (s1)
+	{
+		while (s1[i])
+		{
+			ret[i] = s1[i];
+			i++;
+		}
+	}
+	while (s2[j])
+		ret[i++] = s2[j++];
 	ret[i] = '\0';
 	return (ret);
 }
